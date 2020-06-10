ed94cdbd38369025e5078441cdd3be35d1d47eed
Richard van Hees <r.m.van.hees@sron.nl>
2020-06-10 17:33:14 +0200

    spx_csv2bin_tbl.py: renamed script, improved meta-data, default to add binning table 1, and adds the listed line-skip and flexible binning tables

--- scripts/spx_csv2bin_tbl.py
+++ scripts/spx_csv2bin_tbl.py
@@ -3,7 +3,7 @@ This file is part of pyspex
 
 https://github.com/rmvanhees/pyspex.git
 
-Convert SPEXone binning table in cvs format to netCDF4
+Convert SPEXone binning table in csv format to netCDF4
 
 Copyright (c) 2020 SRON - Netherlands Institute for Space Research
    All Rights Reserved
@@ -42,7 +42,7 @@ def main():
                         help='be verbose, default be silent')
     parser.add_argument('file_list', nargs='+',
                         help=("provide path to SPEXone binning table(s)"
-                              " in cvs-format"))
+                              " in csv-format"))
     args = parser.parse_args()
     if args.verbose:
         print(args)
@@ -62,18 +62,43 @@ def main():
     fid.setncattr('project', 'PACE Project')
     fid.setncattr('instrument', 'SPEXone')
     fid.setncattr('institution',
-                  'SRON Netherlands Institute for Space Research"')
+                  'SRON Netherlands Institute for Space Research')
 
     fid.createDimension('row', 1024)
     fid.createDimension('column', 1024)
 
+    # ----- define tables with table_id equals one -----
+    table_id = 1
+    # write binning table to netCDF4 file
+    gid = fid.createGroup('Table_{:02d}'.format(table_id))
+    gid.enabled_lines = np.uint16(1024)
+    gid.flex_binned_pixels = np.uint32(0)
+
+    dset = gid.createVariable('binning_table', 'u4', ('row', 'column'),
+                              fill_value=fill_value, chunksizes=(128, 128),
+                              zlib=True, complevel=1, shuffle=True)
+    dset.long_name = 'binning table'
+    dset.valid_min = np.uint32(0)
+    dset.valid_max = np.uint32(0xfffff)
+    dset[:] = np.arange(1024 ** 2, dtype='u4').reshape(1024, 1024)
+
+    dset = gid.createVariable('coadding_table', 'u1', ('row', 'column'),
+                              fill_value=0xFF, chunksizes=(128, 128),
+                              zlib=True, complevel=1, shuffle=True)
+    dset.long_name = 'number of coadditions'
+    dset.valid_min = np.uint8(0)
+    dset.valid_max = np.uint8(0xff)
+    dset[:] = np.ones((1024, 1024), dtype='u1')
+    
     # add binning tables
-    for table_id, flname in enumerate(args.file_list):
+    for flname in args.file_list:
         flpath = Path(flname)
         if not flpath.is_file():
             raise FileNotFoundError(flname)
+        if not flpath.suffix == '.csv':
+            continue
 
-        # read cvs-file
+        # read csv-file
         table = np.loadtxt(flname, delimiter=',', dtype=np.uint32)
 
         # determine unique addresses and binning-counts
@@ -84,17 +109,21 @@ def main():
         for ii in indx:
             table[table == ii] = fill_value
 
-        # convert cvs binning table to binning table with FillValues
+        # convert csv binning table to binning table with FillValues
         frame = np.full(table.shape, 0, dtype='u1')
         frame[table != fill_value] = count[table[table != fill_value]]
 
         # write binning table to netCDF4 file
-        gid = fid.createGroup('Table_{:02d}'.format(table_id + 1))
+        table_id += 1
+        gid = fid.createGroup('Table_{:02d}'.format(table_id))
+        gid.origin = flpath.name
+        gid.enabled_lines = np.uint16(np.sum(np.sum(frame, axis=0) > 0))
+        gid.flex_binned_pixels = np.uint32(np.sum(frame > 0))
+
         dset = gid.createVariable('binning_table', 'u4', ('row', 'column'),
                                   fill_value=fill_value, chunksizes=(128, 128),
                                   zlib=True, complevel=1, shuffle=True)
-        dset.comment = 'binning table'
-        dset.origin = flpath.name
+        dset.long_name = 'binning table'
         dset.valid_min = np.uint32(0)
         dset.valid_max = np.uint32(np.max(table[table <= 0x7ffff]))
         dset[:] = table
@@ -102,8 +131,59 @@ def main():
         dset = gid.createVariable('coadding_table', 'u1', ('row', 'column'),
                                   fill_value=0xFF, chunksizes=(128, 128),
                                   zlib=True, complevel=1, shuffle=True)
-        dset.comment = 'number of coadditions'
-        dset.origin = flpath.name
+        dset.long_name = 'number of coadditions'
+        dset.valid_min = np.uint8(0)
+        dset.valid_max = np.uint8(0xff)
+        dset[:] = frame
+
+        if args.figure:
+            plot = S5Pplot(flpath.with_suffix('.pdf').name)
+            plot.draw_signal(table, add_medians=False,
+                             vrange=[0, np.max(table[table <= 0x7ffff])],
+                             title=flpath.name, sub_title='Binning Table')
+            plot.set_cmap(tol_cmap('rainbow_discrete', 5))
+            plot.draw_signal(frame, add_medians=False,
+                             title=flpath.name, sub_title='Number of Coaddings')
+            plot.close()
+
+    # read line-skip definitions
+    for flname in args.file_list:
+        flpath = Path(flname)
+        if not flpath.is_file():
+            raise FileNotFoundError(flname)
+        if not flpath.suffix == '.dat':
+            continue
+
+        # read line-skip definition
+        data = np.loadtxt(flname, delimiter=',', dtype=np.uint8)
+        line_indx = np.where(data == 1)[0]
+        indx = np.arange(1024 * line_indx.size).reshape(line_indx.size, -1)
+
+        table = np.full((1024, 1024), fill_value, dtype='u4')
+        table[line_indx, :] = indx
+
+        frame = np.zeros((1024, 1024), dtype='u1')
+        frame[line_indx, :] = 1
+
+        # write binning table to netCDF4 file
+        table_id += 1
+        gid = fid.createGroup('Table_{:02d}'.format(table_id))
+        gid.origin = flpath.name
+        gid.enabled_lines = np.uint16(line_indx.size)
+        gid.flex_binned_pixels = np.uint32(0)
+
+        dset = gid.createVariable('binning_table', 'u4', ('row', 'column'),
+                                  fill_value=fill_value, chunksizes=(128, 128),
+                                  zlib=True, complevel=1, shuffle=True)
+        dset.long_name = 'binning table'
+        dset.valid_min = np.uint32(0)
+        dset.valid_max = np.uint32(np.max(table[table <= 0x7ffff]))
+        dset[:] = table
+
+        dset = gid.createVariable('coadding_table', 'u1', ('row', 'column'),
+                                  fill_value=0xFF, chunksizes=(128, 128),
+                                  zlib=True, complevel=1, shuffle=True)
+        dset.long_name = 'number of coadditions'
         dset.valid_min = np.uint8(0)
         dset.valid_max = np.uint8(0xff)
         dset[:] = frame
