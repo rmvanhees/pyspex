7d1df74daa3dee28f314e0534e71cc17de0744ab
Richard van Hees <r.m.van.hees@sron.nl>
2020-12-01 16:12:25 +0100

    spx1_add_egse2l1a.py: added the write of EGSE information to a Level-1A product (Todo: selection and waiting for the final format of the EGSE in csv

--- scripts/spx1_add_egse2l1a.py
+++ scripts/spx1_add_egse2l1a.py
@@ -17,6 +17,7 @@ from datetime import datetime, timezone
 # from pathlib import Path
 
 import h5py
+from netCDF4 import Dataset
 import numpy as np
 
 # - global parameters ------------------------------
@@ -37,8 +38,16 @@ def read_egse(egse_file: str, verbose=False):
     """
     Read EGSE data to numpy compound array
     """
-    formats = ('f8',) + 14 * ('f4',) + ('S7',) + 2 * ('i4',)\
-        + 5 * ('f4', 'i2',) + 6 * ('i2',) + ('S5', 'i2')
+    # enumerate source status
+    ldls_dict = {b'UNPLUGGED': 0, b'Controller Fault': 1, b'Idle': 2,
+                 b'Laser ON': 3, b'Lamp ON': 4, b'Missing': 255}
+
+    # enumerate shutter positions
+    shutter_dict = {b'CLOSE': 0, b'OPEN': 1, b'Missing': 255}
+
+    # define dtype of the data 
+    formats = ('f8',) + 14 * ('f4',) + ('u1',) + 2 * ('i4',)\
+        + 5 * ('f4', 'u1',) + 6 * ('u1',) + ('u1',) + ('u1',)
     if verbose:
         print(len(formats), formats)
 
@@ -46,23 +55,67 @@ def read_egse(egse_file: str, verbose=False):
         line = None
         while not line:
             line = fid.readline().strip()
-            names = line.replace('\t', '').split(',')
-            names = [x.strip() for x in names]
-            while names.count(''):
-                names.remove('')
+            fields = line.replace('\t', '').split(',')
+            names = []
+            units = []
+            for field in fields:
+                if field == '':
+                    continue
+                res = field.strip().split(' [')
+                names.append(res[0].replace(' nm', 'nm'))
+                if len(res) == 2:
+                    units.append(res[1].replace('[', '').replace(']', ''))
+                else:
+                    units.append('')
+
         # Temporary fix
         names = tuple(names[:21])\
-            + ('TMC_POS_1 [deg]', 'TMC_MOVING_1',
-               'TMC_POS_2 [deg]', 'TMC_MOVING_2')\
-               + tuple(names[21:])
+            + ('TMC_POS_1', 'TMC_MOVING_1', 'TMC_POS_2', 'TMC_MOVING_2')\
+            + tuple(names[21:])
+        units = tuple(units[:21]) + ('deg', '', 'deg', '') + tuple(units[21:])
         if verbose:
             print(len(names), names)
+            print(len(units), units)
 
         data = np.loadtxt(fid, delimiter=',',
                           dtype={'names': names, 'formats': formats},
-                          converters={0: byte_to_timestamp})
-    return data
+                          converters={0: byte_to_timestamp,
+                                      15: lambda s: ldls_dict[s.strip()],
+                                      34: lambda s: shutter_dict[s.strip()]})
+    return {'values': data, 'units': units,
+            'ldls_dict': ldls_dict, 'shutter_dict': shutter_dict}
 
+def select_egse():
+    return
+
+def write_egse(l1a_file: str, egse, verbose=False):
+    """
+    Read EGSE data to SPEXone Level-1A product
+    """
+    if l1a_file is None:
+        fid = Dataset('test_egse.nc', 'w', format='NETCDF4')
+        gid = fid.createGroup('gse_data')
+    else:
+        fid = Dataset(l1a_file, 'r+', format='NETCDF4')
+        gid = fid['/gse_data']
+
+    ldls_t = gid.createEnumType('u1','ldls_t',
+                                {k.upper(): v
+                                 for k, v in egse['ldls_dict'].items()})
+    shutter_t = gid.createEnumType('u1','shutter_t',
+                                   {k.upper(): v
+                                    for k, v in egse['shutter_dict'].items()})
+    dim_egse = gid.createDimension("egse_packets", egse['values'].size)
+    egse_t = gid.createCompoundType(egse['values'].dtype, 'egse_dtype')
+    var_egse = gid.createVariable("egse", egse_t, ("egse_packets",))
+    var_egse.long_name = 'EGSE settings'
+    var_egse.units = egse['units']
+    var_egse.comment = ('DIG_IN_00 is of enumType ldls_t;'
+                        ' SHUTTER_STATUS is of enumType shutter_t')
+    var_egse[:] = egse['values']
+
+    fid.close()
+    
 
 # - main function ----------------------------------
 def main():
@@ -95,7 +148,9 @@ def main():
     # loop over files with EGSE info until we find a match
     for egse_file in args.file_list:
         egse = read_egse(egse_file, verbose=args.verbose)
-        print(egse['time'].min(), egse['time'].max())
+    print(egse['values']['time'].min(), egse['values']['time'].max())
+
+    write_egse(None, egse, verbose=args.verbose)
 
 
 # --------------------------------------------------
