6d12fb79df42f9f727cf309e24f7196776556820
Richard van Hees <r.m.van.hees@sron.nl>
2020-12-09 19:02:44 +0100

    spx1_add_egse2l1a.py: adjusted EGSE clock to ITOS clock, added EGSE attributes

--- scripts/spx1_add_egse2l1a.py
+++ scripts/spx1_add_egse2l1a.py
@@ -14,24 +14,45 @@ License:  BSD-3-Clause
 import argparse
 
 from datetime import datetime, timedelta, timezone
-# from pathlib import Path
+from pathlib import Path
 
 import h5py
 from netCDF4 import Dataset
 import numpy as np
 
 # - global parameters ------------------------------
-EPOCH = datetime(1970, 1, 1, tzinfo=timezone.utc)
-LEAP_SECONDS = 27
 
 
 # - local functions --------------------------------
+def get_offs_egse_time(sron_sec: float):
+    """
+    Get offset between ITOS and EGSE hardware clocks
+    """
+    date_time_dir = Path('/array/slot1F/spex_one/OCAL/date_stats')
+    if not date_time_dir.is_dir():
+        date_time_dir = Path('/nfs/SPEXone/OCAL/date_stats')
+    if not date_time_dir.is_dir():
+        date_time_dir = Path('/data/richardh/SPEXone/OCAL/date_stats')
+
+    with open(date_time_dir / 'cmp_date_egse_itos.txt', 'r') as fid:
+        names = fid.readline().strip().lstrip(' #').split('\t\t\t')
+        formats = len(names) * ('f8',)
+        data = np.loadtxt(fid, dtype={'names': names, 'formats': formats})
+
+    indx = np.argmin(np.abs(data['SRON'] - sron_sec))
+    # print('argmin: ', indx,
+    #      int(data[indx]['EGSE'] - data[indx]['ITOS']),
+    #      int(data[indx]['SRON'] - data[indx]['EGSE']),
+    #      int(data[indx]['SRON'] - data[indx]['ITOS']))
+    return int(data[indx]['EGSE'] - data[indx]['ITOS'])
+    
+
 def byte_to_timestamp(str_date: str):
     """
     Helper function for numpy.loadtxt() to convert byte-string to timestamp
     """
-    buff = str_date.strip().decode('ascii')
-    return datetime.strptime(buff, '%Y%m%dT%H%M%S.%f').timestamp()
+    buff = str_date.strip().decode('ascii') + '+00:00' # date is in UTC
+    return datetime.strptime(buff, '%Y%m%dT%H%M%S.%f%z').timestamp()
 
 
 def read_egse_old(egse_file: str, verbose=False):
@@ -134,7 +155,7 @@ def read_egse(egse_file: str, verbose=False):
     return {'values': data, 'units': units,
             'ldls_dict': ldls_dict, 'shutter_dict': shutter_dict}
 
-def select_egse(l1a_file: str, egse, verbose=False):
+def select_egse(l1a_file: str, egse, verbose=False, offset=90):
     """
     Return indices EGSE records during the measurement in the Level-1A product
     """
@@ -144,14 +165,23 @@ def select_egse(l1a_file: str, egse, verbose=False):
         coverage_stop = datetime.fromisoformat(
             fid.attrs['time_coverage_end'].decode('ascii'))
 
-    coverage_start += timedelta(seconds=104.468)
-    coverage_stop += timedelta(seconds=104.468)
+    offset = get_offs_egse_time(coverage_start.timestamp()) - 50
+    if verbose:
+        print('offset: ', offset)
+    coverage_start += timedelta(seconds=offset)
+    coverage_stop += timedelta(seconds=offset)
     indx = np.where((egse['values']['time'] >= coverage_start.timestamp())
                     & (egse['values']['time'] <= coverage_stop.timestamp()))[0]
     if indx.size == 0:
         return None
 
+    # perform sanity check
     egse['values'] = egse['values'][indx]
+    for param in ['ALT_ANGLE', 'ACT_ANGLE']:
+        res_sanity = np.all(np.diff(egse['values'][param]) < 0.001)
+        if not res_sanity:
+            print('[WARNING] ', param, egse['values'][param])
+
     return egse
 
 def write_egse(l1a_file: str, egse, verbose=False):
@@ -179,6 +209,34 @@ def write_egse(l1a_file: str, egse, verbose=False):
     var_egse.comment = ('DIG_IN_00 is of enumType ldls_t;'
                         ' SHUTTER_STATUS is of enumType shutter_t')
     var_egse[:] = egse['values']
+
+    if l1a_file is None:
+        fid.close()
+        return
+
+    # investigate filename
+    parts = l1a_file.split('_')
+    act_angle = [x.replace('act', '') for x in parts if x.startswith('act')]
+    alt_angle = [x.replace('alt', '') for x in parts if x.startswith('alt')]
+    
+    view_dict = {'M50DEG': 1, 'M20DEG': 2, '0DEG': 4, 'P20DEG': 8, 'P50DEG': 16}
+    parts_type = parts[2].split('-')
+    gid['viewport'][:] = view_dict.get(parts_type[min(2, len(parts_type))], 255)
+    
+    # write EGSE settings as attributes
+    gid.Line_skip_id = ""
+    gid.Enabled_lines = 2048
+    gid.Binning_table = ""
+    gid.Binned_pixels = 0
+    gid.Light_source = parts[2]
+    gid.FOV_begin = np.nan
+    gid.FOV_end = np.nan
+    gid.ACT_rotationAngle = np.nan if not act_angle else float(act_angle[0])
+    gid.ALT_rotationAngle = np.nan if not alt_angle else float(alt_angle[0])
+    gid.ACT_illumination = np.nan
+    gid.ALT_illumination = np.nan
+    gid.DoLP = 0.
+    gid.AoLP = 0.
 
     fid.close()
 
