f54986115dc6a8a3eea68e4cdefea04f21c2c3e2
Richard van Hees <r.m.van.hees@sron.nl>
2021-01-07 17:24:49 +0100

    spx1_quicklook_lite.py: adopted the Quick-Look script for Science measurements

--- scripts/spx1_quicklook_lite.py
+++ scripts/spx1_quicklook_lite.py
@@ -16,11 +16,62 @@ from datetime import datetime, timedelta
 from pathlib import Path
 
 import h5py
+import numpy as np
 
 from pys5p.lib.plotlib import FIGinfo
 from pys5p.tol_colors import tol_cmap
 from pys5p.s5p_plot import S5Pplot
 
+TEST_BINNING = False
+
+# --------------------------------------------------
+def bin_table_id(binning_table_start) -> int:
+    """
+    Convert the start address of the binning table to an ID [1, 2, ...]
+
+    Notes
+    -----
+    'Diagnostic mode' does not use a binning table. These measurements can be
+    identified by the following register settings: FRAME_MODE=1 & OUTPMODE=3
+
+    'Science mode' using binning table to decrease the data rate. These
+    measurements  can be identified by the following register settings:
+    FRAME_MODE=2 & OUTPMODE=1
+
+    The first binning table is written to address 0x80000000, each table has
+    a size of 1024x1024 pointers of 4 bytes (0x400000)
+    """
+    return 1 + (binning_table_start - 0x80000000) // 0x400000
+
+
+def binned_to_2x2_image(table_id: int, img_data):
+    """
+    Convert binned detector data to 2x2 binned image (1024x1024)
+    """
+    ckd_dir = '/nfs/SPEXone/share/ckd'
+    if not Path(ckd_dir).is_dir():
+        ckd_dir = '/data/richardh/SPEXone/share/ckd'
+
+    # only read the latest version of the binning-table CKD
+    bin_tbl_ckd = sorted(list(Path(ckd_dir).glob('SPX1_OCAL_L1A_TBL_*.nc')))
+    if not bin_tbl_ckd:
+        raise FileNotFoundError('No CKD with binning tables found')
+
+    with h5py.File(bin_tbl_ckd[-1], 'r') as fid:
+        coad_table = fid['/Table_{:02d}/coadding_table'.format(table_id)][:]
+
+    # write binned data to 2-D grid
+    data = np.zeros((1024, 1024), dtype='f4')
+    mask = coad_table > 0
+    data[mask] = img_data
+
+    # correct data for number of coaddings (pixel dependent)
+    data[mask] /= coad_table[mask]
+
+    return data
+
+
+# --------------------------------------------------
 def main():
     """
     Main function of this module
@@ -35,12 +86,19 @@ def main():
 
         with h5py.File(flname, 'r') as fid:
             date_start = fid.attrs['time_coverage_start']
-            image_time = fid['image_attributes/image_time'][:]
-            nr_coadditions = fid['image_attributes/nr_coadditions'][:]
-            exposure_time = fid['image_attributes/exposure_time'][:]
-            images = fid['science_data/detector_images'][:] \
-                / nr_coadditions[:, None]
+            image_time = fid['/image_attributes/image_time'][:]
+            exposure_time = fid['/image_attributes/exposure_time'][:]
+            sci_hk = fid['/science_data/detector_telemetry'][:]
+            images = fid['/science_data/detector_images'][:]
 
+        if TEST_BINNING:
+            sci_hk['REG_FULL_FRAME'] = 2
+            sci_hk['REG_CMV_OUTPUTMODE'] = 1
+            sci_hk['REG_BINNING_TABLE_START'] = 0x80000000 + 0x400000
+            # de gebruikte binning tabel bevat data uit 416023 pixels
+            # dus selecteer ik net zoveel pixels uit het full-frame image
+            images = images[:, 20480:20480+416023]
+                
         try:
             date_start = date_start.decode()
         except (UnicodeDecodeError, AttributeError):
@@ -55,8 +113,17 @@ def main():
         plot = S5Pplot((data_dir / flname.name).with_suffix('.pdf'))
         plot.set_cmap(tol_cmap('rainbow_WhBr_condense'))
         for ii, img in enumerate(images):
-            if img.size != 4194304:
-                continue
+            if sci_hk[ii]['REG_FULL_FRAME'] == 2 \
+               and sci_hk[ii]['REG_CMV_OUTPUTMODE'] == 1:
+                table_id = bin_table_id(sci_hk[ii]['REG_BINNING_TABLE_START'])
+                img = binned_to_2x2_image(table_id, img)
+            elif sci_hk[ii]['REG_FULL_FRAME'] == 1 \
+                 and sci_hk[ii]['REG_CMV_OUTPUTMODE'] == 3:
+                if img.size != 4194304:
+                    continue
+                img = img.reshape(2048, 2048) / sci_hk[ii]['REG_NCOADDFRAMES']
+            else:
+                raise RuntimeError('unknown FRAME_MODE, OUTPMODE combination')
 
             time_str = (
                 datetime(year=2020, month=1, day=1)
@@ -68,7 +135,7 @@ def main():
             figinfo.add('exposure_time', exposure_time[ii], fmt='{:f}s')
             figinfo.add('signal_range', [int(img.min()), int(img.max())],
                         fmt='{}')
-            plot.draw_signal(img.reshape(2048, 2048), vperc=[1, 99],
+            plot.draw_signal(img, vperc=[1, 99],
                              fig_info=figinfo, sub_title='frame: {}'.format(ii))
 
         plot.close()
