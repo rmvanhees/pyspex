03a72390acb69fe20f3cd81e64f4dbfee1767270
Richard van Hees <r.m.van.hees@sron.nl>
2021-02-26 12:03:07 +0100

    lv1_gse.py: renamed l1a_gse to lv1_gse; write attribute when adding group gse_data; added check on EGSE data; fixed several bugs

--- src/pyspex/lv1_gse.py
+++ src/pyspex/lv1_gse.py
@@ -15,7 +15,7 @@ import numpy as np
 from netCDF4 import Dataset
 
 
-class L1Agse:
+class LV1gse:
     """
     Python class add EGSE/OGSE data to a SPEXone Level-1A product
     """
@@ -25,25 +25,50 @@ class L1Agse:
         """
         self.fid = Dataset(l1a_file, 'r+')
 
-        if "gse_data" not in self.fid.groups.values():
-            gid = self.fid.createGroup('/gse_data')
+        if self.fid.groups.get("gse_data"):
+            return
 
-            dset = gid.createVariable('viewport', 'u1')
-            dset.long_name = "viewport status"
-            dset.standard_name = "status_flag"
-            dset.valid_range = np.array([0, 16], dtype='u1')
-            dset.flag_values = np.array([0, 1, 2, 4, 8, 16], dtype='u1')
-            dset.flag_meanings = "ALL -50deg -20deg 0deg +20deg +50deg"
-            dset[:] = 0   # initialize to default value: all viewports used
+        # investigate filename
+        parts = l1a_file.split('_')
+        act_angle = [float(x.replace('act', ''))
+                     for x in parts if x.startswith('act')]
+        alt_angle = [float(x.replace('alt', ''))
+                     for x in parts if x.startswith('alt')]
+        pol_angle = [float(x.replace('pol', ''))
+                     for x in parts if x.startswith('pol')]
 
-            gid.FOV_begin = np.nan
-            gid.FOV_end = np.nan
-            gid.ACT_rotationAngle = np.nan
-            gid.ALT_rotationAngle = np.nan
-            gid.ACT_illumination = np.nan
-            gid.ALT_illumination = np.nan
+        # determine viewport: default 0, when all viewports are illuminated
+        if alt_angle:
+            vp_dict = {'-50.0': 1, '-20.0': 2, '0.0': 4, '20.0': 8, '50.0': 16}
+
+            viewport = vp_dict.get('{:.1f}'.format(alt_angle[0]), 0)
+        else:
+            vp_dict = {'M50DEG': 1, 'M20DEG': 2, '0DEG': 4, 'P20DEG': 8,
+                       'P50DEG': 16}
+            vp_parts = parts[2].split('-')
+            viewport = vp_dict.get(vp_parts[min(2, len(vp_parts))], 0)
+
+        gid = self.fid.createGroup('/gse_data')
+        dset = gid.createVariable('viewport', 'u1')
+        dset.long_name = "viewport status"
+        dset.standard_name = "status_flag"
+        dset.valid_range = np.array([0, 16], dtype='u1')
+        dset.flag_values = np.array([0, 1, 2, 4, 8, 16], dtype='u1')
+        dset.flag_meanings = "ALL -50deg -20deg 0deg +20deg +50deg"
+        dset[:] = viewport
+
+        # gid.FOV_begin = np.nan
+        # gid.FOV_end = np.nan
+        gid.ACT_rotationAngle = np.nan if not act_angle else act_angle[0]
+        gid.ALT_rotationAngle = np.nan if not alt_angle else alt_angle[0]
+        # gid.ACT_illumination = np.nan
+        # gid.ALT_illumination = np.nan
+        gid.AoLP = 0. if not pol_angle else pol_angle[0]
+        fields = parts[0].split('-')
+        if fields[0] in ('POLARIZED', 'POLARIMETRIC') and fields[1] != 'BKG':
+            gid.DoLP = 1.
+        else:
             gid.DoLP = 0.
-            gid.AoLP = 0.
 
     def __iter__(self) -> None:
         for attr in sorted(self.__dict__):
@@ -75,6 +100,8 @@ class L1Agse:
 
     def write_viewport(self, viewport: int) -> None:
         """
+        Add/update which viewports are illuminated
+
         Parameters
         ----------
         viewport :  int
@@ -105,12 +132,17 @@ class L1Agse:
 
     def write_egse(self, egse_time, egse_data, egse_attrs: dict) -> None:
         """
+        Add EGSE parameters
+
         Parameters
         ----------
         egse_time :  ndarray
         egse_data :  ndarray
         egse_attrs :  dict
         """
+        # perform sanity check on EGSE parameters
+        self.check_egse(egse_data)
+
         gid = self.fid['/gse_data']
         _ = gid.createDimension('time', len(egse_data))
         dset = gid.createVariable('time', 'f8', ('time',))
@@ -124,6 +156,8 @@ class L1Agse:
     def write_reference_diode(self, ref_time, ref_data,
                               ref_attrs: dict) -> None:
         """
+        Add data from the reference diode
+
         Parameters
         ----------
         ref_time :  ndarray
@@ -144,6 +178,8 @@ class L1Agse:
     def write_wavelength_monitor(self, wav_time, wav_intg, wav_avg_num,
                                  wav_wv, wav_signal) -> None:
         """
+        Add wavelength monitoring data of the Avantas fibre-spectrometer
+
         Parameters
         ----------
         wav_time :  ndarray
@@ -180,18 +216,22 @@ class L1Agse:
         dset.comment = 'Avantes fibre spectrometer'
         dset[:] = wav_signal
 
-    def write_attr_fov(self, val_begin: float, val_end: float) -> None:
-        """
-        Parameters
-        ----------
-        val_begin :  float
-        val_end :  float
-        """
-        self.fid['/gse_data'].FOV_begin = val_begin
-        self.fid['/gse_data'].FOV_end = val_end
+    # def write_attr_fov(self, val_begin: float, val_end: float) -> None:
+    #    """
+    #    Add field-of-view range as an group attribute
+    #
+    #    Parameters
+    #    ----------
+    #    val_begin :  float
+    #    val_end :  float
+    #    """
+    #    self.fid['/gse_data'].FOV_begin = val_begin
+    #    self.fid['/gse_data'].FOV_end = val_end
 
     def write_attr_act(self, angle: float, illumination=None) -> None:
         """
+        Add act rotation angle as an group attribute
+
         Parameters
         ----------
         angle :  float
@@ -202,6 +242,8 @@ class L1Agse:
 
     def write_attr_alt(self, angle: float, illumination=None) -> None:
         """
+        Add altitude rotation angle as an group attribute
+
         Parameters
         ----------
         angle :  float
@@ -212,6 +254,8 @@ class L1Agse:
 
     def write_attr_polarization(self, aolp: float, dolp: float) -> None:
         """
+        Add polarization parameters AoLP & DoLP as an group attribute
+
         Parameters
         ----------
         AoLP :  float
@@ -222,10 +266,30 @@ class L1Agse:
         if dolp is not None:
             self.fid['/gse_data'].DoLP = dolp
 
+    def check_egse(self, egse_data):
+        """
+        Check consistency of OGSE/EGSE information during measurement
+        """
+        for key, fmt in egse_data.dtype.fields.items():
+            if fmt[0] == np.uint8:
+                res_sanity = (egse_data[key] == egse_data[key][0]).all()
+                if not res_sanity:
+                    print('[WARNING] ', key, egse_data[key])
+
+        act_angle = self.fid['/gse_data'].ACT_rotationAngle
+        if np.isfinite(act_angle):
+            if not np.allclose(egse_data['ACT_ANGLE'], act_angle, 1e-2):
+                print('[WARNING] ', 'ACT_ANGLE', egse_data['ACT_ANGLE'])
+
+        alt_angle = self.fid['/gse_data'].ALT_rotationAngle
+        if alt_angle:
+            if not np.allclose(egse_data['ALT_ANGLE'], alt_angle, 1e-2):
+                print('[WARNING] ', 'ALT_ANGLE', egse_data['ALT_ANGLE'])
+
 
 #    def fill_gse(self, reference=None) -> None:
 #        """
-#        Write EGSE/OGSE data to L1A product
+#        Write EGSE/OGSE data to a SPEXone Level-1A product
 #
 #        Parameters
 #        ----------
