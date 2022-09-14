56b51fe587337bc1e66759cc08068a5e25b6eb5f
Richard van Hees <r.m.van.hees@sron.nl>
2022-09-14 17:27:06 +0200

    ckd_io.py: rewrote class to read the official L1B CKD

--- src/pyspex/ckd_io.py
+++ src/pyspex/ckd_io.py
@@ -3,64 +3,39 @@ This file is part of pyspex
 
 https://github.com/rmvanhees/pyspex.git
 
-Python implementation to create the SPEXone CKD product, or update/read CKD
-parameters
+Python implementation to read the SPEXone CKD product.
 
-Copyright (c) 2019-2021 SRON - Netherlands Institute for Space Research
+Copyright (c) 2019-2022 SRON - Netherlands Institute for Space Research
    All Rights Reserved
 
 License:  BSD-3-Clause
 """
-from datetime import datetime
+from datetime import datetime, timezone
 from pathlib import Path
 # import argparse
 
 import h5py
-import numpy as np
+import xarray as xr
 
-from pyspex import version
+from moniplot.image_to_xarray import h5_to_xr
+
 
 # - global parameters ------------------------------
-VERSION = '000100'
 
 
 # - local functions --------------------------------
-def str_netcdf():
-    """
-    return string to identify a netCDF dimension
-    """
-    return 'This is a netCDF dimension but not a netCDF variable.'
 
 
 # - class CKDio -------------------------
 class CKDio:
     """
-    Defines class to read SPEXone CKD parameters
+    Defines a class to read SPEXone CKD parameters
 
     Attributes
     ----------
-    filename :  str
-    offset : int
-    verbose : bool
 
     Methods
     -------
-    add_offset(values, binning=-1)
-       Add Offset CKD to CKD product.
-    add_darkcur(values, binning=-1)
-       Add Dark-current CKD to CKD product.
-    add_non_linearity(values, binning=-1)
-       Add non-linearity CKD to CKD product.
-    add_prnu(values, binning=-1)
-       Add PRNU CKD to CKD product.
-    get_offset(binning=-1)
-       Read Offset CKD.
-    get_darkcur(binning=-1)
-       Read Dark-current CKD.
-    get_non_linearity(values, binning=-1)
-       Read non-Linearity CKD.
-    get_prnu(binning=-1)
-       Read PRNU CKD.
 
     Notes
     -----
@@ -68,7 +43,7 @@ class CKDio:
     Examples
     --------
     """
-    def __init__(self, ckd_file, verbose=False):
+    def __init__(self, ckd_file: str, verbose=False) -> None:
         """
         Initialize class attributes
 
@@ -76,229 +51,207 @@ class CKDio:
         ----------
         ckd_file :  str
            Name of CKD file
-        verbose :  bool
+        verbose :  bool, default=False
            Be verbose
         """
-        self.filename = ckd_file
-        self.offset = 0
+        if not Path(ckd_file).is_file():
+            raise FileNotFoundError(f'{ckd_file} does not exist')
         self.verbose = verbose
 
-    def __initialize_product(self, valid_date=None, overwrite=False):
+        # open access to CKD product
+        self.fid = h5py.File(ckd_file, "r")
+        if 'processor_configuration' not in self.fid:
+            raise RuntimeError('CKD product in complete?')
+
+    def __enter__(self):
         """
-        Initialize SPEXone product with CKD parameters
-
-        Define name of SPEXone CKD product as:
-           SPX1_L1_CKD_<date_valid>_<version>_<date_created>.h5
-        - date_valid : ISO UTC date-time as yymmddTHHMMSS
-        - version : 6 digits as MMmmrr (for Major, minor and release versions)
-        - date_created : ISO UTC date-time as yymmddTHHMMSS
+        method called to initiate the context manager
         """
-        if valid_date is None:
-            valid_date = '20191001T000000'
+        return self
 
-        creation_date = datetime.utcnow().strftime("%Y%m%dT%H%M%S")
-        self.filename = \
-            f'SPX1_L1_CKD_{valid_date:13s}_{VERSION:6s}_{creation_date:13s}.h5'
-
-        if Path(self.filename).is_file():
-            if not overwrite:
-                raise FileExistsError
-            Path(self.filename).unlink()
-
-        with h5py.File(self.filename, 'w') as fid:
-            fid.attrs['title'] = 'SPEXone Level-1 CKD product'
-            fid.attrs['comment'] = ''
-            fid.attrs['name'] = self.filename
-            fid.attrs['type'] = 'CKD product'
-            fid.attrs['mission'] = 'PACE'
-            fid.attrs['instrument'] = 'SPEXone'
-            fid.attrs['creator'] = \
-                'SRON Netherlands Institute for Space Research'
-            fid.attrs['creator_date'] = creation_date
-            fid.attrs['creator_version'] = VERSION
-            fid.attrs['sw_version'] = version.get()
-            fid.attrs['valitity_start'] = valid_date
-            fid.attrs['validity_stop'] = '20500101T000000'
-
-            grp = fid.create_group('/FULL_FRAME')
-            # create dimensions
-            dset = grp.create_dataset('row', (2048,), dtype='u2')
-            dset.make_scale(str_netcdf())
-            dset.attrs['long_name'] = 'row'
-            dset[:] = np.arange(0, 2048, dtype='u2')
-
-            dset = grp.create_dataset('column', (2048,), dtype='u2')
-            dset.make_scale(str_netcdf())
-            dset.attrs['long_name'] = 'column'
-            dset[:] = np.arange(0, 2048, dtype='u2')
-
-            _ = fid.create_group('/BINNING_00')
-            # create dimensions of default binning scheme
-
-    def add_offset(self, values, binning=-1):
+    def __exit__(self, exc_type, exc_value, traceback):
         """
-        Add Offset CKD to CKD product
+        method called when exiting the context manager
         """
-        if self.filename is None or not Path(self.filename).is_file():
-            self.__initialize_product()
+        self.close()
+        return False  # any exception is raised by the with statement.
 
-        with h5py.File(self.filename, 'r+') as fid:
-            if binning == -1:
-                grp = fid['/FULL_FRAME']
-            else:
-                grp = fid[f'/BINNING_{binning:03d}']
-            if 'offset' in fid:
-                raise ValueError('dataset offset already exists')
-
-            # pylint: disable=no-member
-            frame_shape = (grp['row'].size, grp['column'].size)
-            dset = grp.create_dataset('offset', frame_shape,
-                                      compression=1, shuffle=True,
-                                      fillvalue=np.nan, dtype='f4')
-            dset.dims[0].attach_scale(grp['row'])
-            dset.dims[1].attach_scale(grp['column'])
-            dset.attrs['long_name'] = "Offset"
-            dset[:] = values
-            dset.attrs['date_created'] = \
-                datetime.utcnow().isoformat(timespec='milliseconds')
-
-    def add_darkcur(self, values, binning=-1):
+    def close(self) -> None:
         """
-        Add Dark-current CKD to CKD product
+        Make sure that we close all resources
         """
-        if self.filename is None or not Path(self.filename).is_file():
-            self.__initialize_product()
+        if self.fid is not None:
+            self.fid.close()
 
-        with h5py.File(self.filename, 'r+') as fid:
-            if binning == -1:
-                grp = fid['/FULL_FRAME']
-            else:
-                grp = fid[f'/BINNING_{binning:03d}']
-            if 'darkcurrent' in fid:
-                raise ValueError('dataset darkcurrent already exists')
-
-            # pylint: disable=no-member
-            frame_shape = (grp['row'].size, grp['column'].size)
-            dset = grp.create_dataset('darkcurrent', frame_shape,
-                                      compression=1, shuffle=True,
-                                      fillvalue=np.nan, dtype='f4')
-            dset.dims[0].attach_scale(grp['row'])
-            dset.dims[1].attach_scale(grp['column'])
-            dset.attrs['long_name'] = "Dark-current"
-            dset[:] = values
-            dset.attrs['date_created'] = \
-                datetime.utcnow().isoformat(timespec='milliseconds')
-
-    def add_non_linearity(self, values, binning=-1):
+    @property
+    def processor_version(self) -> str:
         """
-        Add non-linearity CKD to CKD product
-
-        Notes
-        -----
-        Not implemented
+        Return the version of the spexone_cal program
         """
-        if self.filename is None or not Path(self.filename).is_file():
-            self.__initialize_product()
+        # pylint: disable=no-member
+        return self.fid.attrs['processor_version'].decode()
 
-        with h5py.File(self.filename, 'r+') as fid:
-            if binning == -1:
-                grp = fid['/FULL_FRAME']
-            else:
-                grp = fid[f'/BINNING_{binning:03d}']
-            if 'nonlinearity' in fid:
-                raise ValueError('dataset nonlinearity already exists')
-            raise ValueError('not yet implemented')
-
-    def add_prnu(self, values, binning=-1):
+    @property
+    def date_created(self) -> str:
         """
-        Add PRNU CKD to CKD product
+        Return creation date of the CKD product
         """
-        if self.filename is None or not Path(self.filename).is_file():
-            self.__initialize_product()
+        # pylint: disable=no-member
+        date_str = self.fid.attrs['date_created'].decode()
+        date_t = datetime.strptime(date_str, "%Y %B %d %a %Z%z %H:%M:%S")
+        return date_t.astimezone(tz=timezone.utc).isoformat()[:-6]
 
-        with h5py.File(self.filename, 'r+') as fid:
-            if binning == -1:
-                grp = fid['/FULL_FRAME']
-            else:
-                grp = fid[f'/BINNING_{binning:03d}']
-            if 'PRNU' in fid:
-                raise ValueError('dataset PRNU already exists')
-
-            # pylint: disable=no-member
-            frame_shape = (grp['row'].size, grp['column'].size)
-            dset = grp.create_dataset('PRNU', frame_shape,
-                                      compression=1, shuffle=True,
-                                      fillvalue=np.nan, dtype='f4')
-            dset.dims[0].attach_scale(grp['row'])
-            dset.dims[1].attach_scale(grp['column'])
-            dset.attrs['long_name'] = "Pixel Response Non-Uniformity"
-            dset[:] = values
-            dset.attrs['date_created'] = \
-                datetime.utcnow().isoformat(timespec='milliseconds')
-
-    def get_offset(self, binning=-1):
+    @property
+    def git_commit(self) -> str:
         """
-        Read Offset CKD
+        Return git hash of repository spexone_cal, used to generate the CKD
         """
-        with h5py.File(self.filename, 'r') as fid:
-            if binning == -1:
-                grp = fid['/FULL_FRAME']
-            else:
-                grp = fid[f'/BINNING_{binning:03d}']
-            if 'offset' not in grp:
-                raise ValueError('CKD offset does not exist')
+        # pylint: disable=no-member
+        return self.fid.attrs['git_commit'].decode()
 
-            values = grp['offset'][:]
-
-        return values
-
-    def get_darkcur(self, binning=-1):
+    def dark(self) -> xr.Dataset:
         """
-        Read Dark-current CKD
+        Read Dark CKD
+
+        Returns
+        -------
+        xr.Dataset
         """
-        with h5py.File(self.filename, 'r') as fid:
-            if binning == -1:
-                grp = fid['/FULL_FRAME']
-            else:
-                grp = fid[f'/BINNING_{binning:03d}']
-            if 'darkcurrent' not in grp:
-                raise ValueError('CKD darkcurrent does not exist')
+        try:
+            gid = self.fid['DARK']
+        except KeyError:
+            return None
+        res = ()
+        buff = h5_to_xr(gid['dark_offset'])
+        buff.name = 'offset'
+        res += (buff,)
+        buff = h5_to_xr(gid['dark_current'])
+        buff.name = 'current'
+        res += (buff,)
+        return xr.merge(res, combine_attrs='drop_conflicts')
 
-            values = grp['darkcurrent'][:]
-
-        return values
-
-    def get_non_linearity(self, values, binning=-1):
+    def noise(self) -> xr.Dataset:
         """
-        Read non-Linearity CKD
+        Read Noise CKD
+
+        Returns
+        -------
+        xr.Dataset
         """
-        with h5py.File(self.filename, 'r') as fid:
-            if binning == -1:
-                grp = fid['/FULL_FRAME']
-            else:
-                grp = fid[f'/BINNING_{binning:03d}']
-            if 'nonlinearity' not in grp:
-                raise ValueError('CKD nonlinearity does not exist')
+        try:
+            gid = self.fid['NOISE']
+        except KeyError:
+            return None
+        res = ()
+        res += (h5_to_xr(gid['g']),)
+        res += (h5_to_xr(gid['n']),)
+        return xr.merge(res, combine_attrs='drop_conflicts')
 
-            values = grp['nonlinearity'][:]
+    def nlin(self) -> xr.Dataset:
+        """
+        Read non-linearity CKD
 
-        return values
+        Returns
+        -------
+        xr.Dataset
+        """
+        try:
+            gid = self.fid['NON_LINEARITY']
+        except KeyError:
+            return None
+        res = ()
+        res += (h5_to_xr(gid['nonlin_order']),)
+        res += (h5_to_xr(gid['nonlin_knots']),)
+        res += (h5_to_xr(gid['nonlin_exptimes']),)
+        res += (h5_to_xr(gid['nonlin_signal_scale']),)
+        res += (h5_to_xr(gid['nonlin_fit']),)
+        return xr.merge(res, combine_attrs='drop_conflicts')
 
-    def get_prnu(self, binning=-1):
+    def prnu(self) -> xr.DataArray:
         """
         Read PRNU CKD
+
+        Returns
+        -------
+        xr.DataArray
         """
-        with h5py.File(self.filename, 'r') as fid:
-            if binning == -1:
-                grp = fid['/FULL_FRAME']
-            else:
-                grp = fid[f'/BINNING_{binning:03d}']
-            if 'PRNU' not in grp:
-                raise ValueError('CKD PRNU does not exist')
+        try:
+            gid = self.fid['PRNU']
+        except KeyError:
+            return None
+        return h5_to_xr(gid['prnu'])
 
-            values = grp['PRNU'][:]
+    def fov(self) -> xr.Dataset:
+        """
+        Read field-of-view CKD
 
-        return values
+        Returns
+        -------
+        xr.Dataset
+        """
+        try:
+            gid = self.fid['FIELD_OF_VIEW']
+        except KeyError:
+            return None            
+        res = ()
+        res += (h5_to_xr(gid['fov_nfov_vp']),)
+        res += (h5_to_xr(gid['fov_ifov_start_vp']),)
+        res += (h5_to_xr(gid['fov_act_angles']),)
+        res += (h5_to_xr(gid['fov_ispat']),)
+        return xr.merge(res, combine_attrs='drop_conflicts')
+
+    def wavelength(self) -> xr.Dataset:
+        """
+        Read Wavelength CKD
+
+        Returns
+        -------
+        xr.Dataset
+        """
+        try:
+            gid = self.fid['WAVELENGTH']
+        except KeyError:
+            return None
+        res = ()
+        buff = h5_to_xr(gid['wave_full'])
+        buff.name = 'full'
+        res += (buff,)
+        buff = h5_to_xr(gid['wave_common'])
+        buff.name = 'common'
+        res += (buff,)
+        return xr.merge(res, combine_attrs='drop_conflicts')
+
+    def radiometric(self) -> xr.DataArray:
+        """
+        Read Radiometric CKD
+
+        Returns
+        -------
+        xr.DataArray
+        """
+        try:
+            gid = self.fid['RADIOMETRIC']
+        except KeyError:
+            return None
+        return h5_to_xr(gid['rad_spectra'])
+
+    def polarimetric(self) -> xr.Dataset:
+        """
+        Read Polarimetric CKD
+
+        Returns
+        -------
+        xr.Dataset
+        """
+        try:
+            gid = self.fid['POLARIMETRIC']
+        except KeyError:
+            return None
+        res = ()
+        res += (h5_to_xr(gid['pol_m_q']),)
+        res += (h5_to_xr(gid['pol_m_u']),)
+        res += (h5_to_xr(gid['pol_m_t']),)
+        return xr.merge(res, combine_attrs='drop_conflicts')
 
 
 # - main function ----------------------------------
@@ -306,15 +259,19 @@ def main():
     """
     main function
     """
-    ckd = CKDio(None, verbose=True)
-    ckd.add_offset(np.zeros((2048, 2048), dtype=float))
-    ckd.add_darkcur(np.zeros((2048, 2048), dtype=float))
-    ckd.add_prnu(np.ones((2048, 2048), dtype=float))
-
-    print(np.mean(ckd.get_offset()))
-    print(np.mean(ckd.get_darkcur()))
-    print(np.mean(ckd.get_prnu()))
-
+    ckd_dir = '/data/richardh/SPEXone/share/ckd/'
+    ckd_file = 'CKD_reference_all_20220719_145838.nc'
+    with CKDio(ckd_dir + ckd_file) as ckd:
+        print(ckd.processor_version)
+        print(ckd.date_created)
+        print(ckd.dark())
+        print(ckd.noise())
+        print(ckd.nlin())
+        print(ckd.prnu())
+        print(ckd.fov())
+        print(ckd.wavelength())
+        print(ckd.radiometric())
+        print(ckd.polarimetric())
 
 # --------------------------------------------------
 if __name__ == '__main__':
