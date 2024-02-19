d6238885cce4413893d7588ffc05cf46f1d302a7
Richard van Hees <r.m.van.hees@sron.nl>
2024-02-19 19:16:33 +0100

    Fixed coversion of heater current to mA

--- src/pyspex/lib/tlm_utils.py
+++ src/pyspex/lib/tlm_utils.py
@@ -142,7 +142,7 @@ def poly_spex_icuhk_internaltemp2(raw_data: np.ndarray) -> np.ndarray:
 
 def poly_spex_htr_v(raw_data: np.ndarray) -> np.ndarray:
     """Convert Heater Bus voltages to Volt."""
-    coefficients = (0, 0.003)
+    coefficients = (0, 0.01 / 3)
     return coefficients[0] + coefficients[1] * raw_data
 
 
